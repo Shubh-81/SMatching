e4bedffdefad94f187d1bd72595ad933bf228745
Shubh-81 <shubh57ag@gmail.com>
2023-03-30 01:29:09 +0530

    Should Work

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -13,20 +13,50 @@ export const register = async (req,res) => {
             picturePath,
             insta_id
         } = req.body;
-
-        const salt = await bcrpyt.genSalt();
-        const passwordHash = await bcrpyt.hash(password,salt);
-        const newUser = new User({
-            firstName,
-            lastName,
-            email,
-            mobileNo,
-            password: passwordHash,
-            picturePath,
-            insta_id,
+        const results = await User.find();
+        var foundUser;
+        results.map((user)=>{
+            if(user.email==email!="" || user.mobileNo==mobileNo!="" || user.insta_id==insta_id!="") {
+                foundUser = user;
+                return;
+            }
         });
-        const savedUser = await newUser.save();
-        res.status(201).json(savedUser);
+        console.log(foundUser);
+        if(!foundUser) {
+            const salt = await bcrpyt.genSalt();
+            const passwordHash = await bcrpyt.hash(password,salt);
+            const newUser = new User({
+                firstName,
+                lastName,
+                email,
+                mobileNo,
+                password: passwordHash,
+                picturePath,
+                insta_id,
+            });
+            const savedUser = await newUser.save();
+            res.status(201).json(savedUser);
+        }
+        else {
+            if(foundUser.isUser) {
+                res.status(400).json({message: "User with one of unique entries already exsists"});
+            } else {
+                const salt = await bcrpyt.genSalt();
+                const passwordHash = await bcrpyt.hash(password,salt);
+                const saved = await foundUser.updateOne(
+                    {
+                        firstName: firstName,
+                        lastName: lastName,
+                        email: email,
+                        mobileNo: mobileNo,
+                        password: passwordHash,
+                        picturePath: picturePath,
+                        insta_id: insta_id,
+                        isUser: true
+                    });
+                res.status(200).json(saved);
+            }
+        }
     }   catch(err) {
         res.status(500).json({error: err.message}); 
     }
