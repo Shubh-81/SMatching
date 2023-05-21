4ffda04c60cb1e8f0544799e2c54d5833fd02312
Shubh-81 <shubh57ag@gmail.com>
2023-05-21 15:48:16 +0530

    Vercel Analyst

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,9 @@ import { useSelector } from "react-redux";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { createTheme } from "@mui/material/styles";
 import { themeSettings } from "./theme";
+import { inject } from '@vercel/analytics';
+ 
+inject();
 
 function App() {
   const mode = useSelector((state) => state.mode);
